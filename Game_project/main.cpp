317efcc4e0bcccbf8eb4183aac13761f1976ab5d
t0npl <69316922+t0npl@users.noreply.github.com>
2020-11-09 10:27:45 +0700

    -

--- Game_project/main.cpp
+++ Game_project/main.cpp
@@ -1,58 +1,63 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <math.h>
+using namespace sf;
 int main() {
+	//renderwindow
 	sf::RenderWindow window(sf::VideoMode(1200, 600), "Pachara Loawpetch 63010629");
-	//////bg
+	//bgtexture
 	sf::Texture backgroundTexture;
 	if (!backgroundTexture.loadFromFile("bg.png"))
 	{
 		std::cout << "load failed" << std::endl;
 	}
-	//////Texture
+	//playertexture
 	sf::Texture playerTexture;
 	if (!playerTexture.loadFromFile("player_plane.png"))
 	{
 		std::cout << "load failed" << std::endl;
 	}
-	//////Fire
+	//firetexture
 	sf::Texture fireTexture;
 	if (!fireTexture.loadFromFile("fire.png"))
 	{
 		std::cout << "load failed" << std::endl;
 	}
-	//////bg
+	//bg
 	sf::Sprite backgroundSprite;
 	backgroundSprite.setTexture(backgroundTexture);
-	//////Sprite
-	sf::Sprite shapeSprite;
-	shapeSprite.setTexture(playerTexture);
+	//player
+	sf::Sprite playerSprite;
+	playerSprite.setTexture(playerTexture);
 	int spriteSizeX = playerTexture.getSize().x/3;
 	int spriteSizeY = playerTexture.getSize().y/2;
-	shapeSprite.setTextureRect(sf::IntRect(0, 0, spriteSizeX, spriteSizeY));
-	shapeSprite.setPosition(150,50);
-	//////fireSprite
+	playerSprite.setTextureRect(sf::IntRect(0, 0, spriteSizeX, spriteSizeY));
+	playerSprite.setPosition(150,50);
+	//fire
 	sf::Sprite fireSprite;
 	fireSprite.setTexture(fireTexture);
 	int fspriteSizeX = playerTexture.getSize().x / 4;
 	int fspriteSizeY = playerTexture.getSize().y / 1;
 	fireSprite.setTextureRect(sf::IntRect(0, 0, fspriteSizeX, fspriteSizeY));
 	fireSprite.setPosition(90, 57);
-	///////statements
+	//generalvariables
 	int animationFrame = 0;
 	int fireframe=0;
 	float yBorder=50;
+	int movement = 0;
+	//gameloop
 	while (window.isOpen())
-	{
-		int movement=0;
+	{	
+		movement = 0;
 		window.draw(backgroundSprite);
-		window.draw(shapeSprite);
+		window.draw(playerSprite);
 		window.draw(fireSprite);
 		window.display();
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) 
 		{
 			movement = 2;
 		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
 		{
 			movement = 1;
 		}
@@ -60,21 +65,20 @@ int main() {
 		{
 			window.close();
 		}
-			if (movement == 0) {
-				shapeSprite.setTextureRect(sf::IntRect(0, 0, spriteSizeX, spriteSizeY));
+		if (movement == 0) {
+			playerSprite.setTextureRect(sf::IntRect(0, 0, spriteSizeX, spriteSizeY));
 			}
 			else if (movement == 2 && yBorder != 530) {
-				shapeSprite.move(0.f, 1.f);
+				playerSprite.move(0.f, 1.f);
 				fireSprite.move(0.f, 1.f);
 				yBorder += 1;
-				shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 1, spriteSizeX, spriteSizeY));
-
+				playerSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 1, spriteSizeX, spriteSizeY));
 			}
 			else if (movement == 1 && yBorder != 0) {
-				shapeSprite.move(0.f, -1.f);
+				playerSprite.move(0.f, -1.f);
 				fireSprite.move(0.f, -1.f);
 				yBorder -= 1;
-				shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, 0, spriteSizeX, spriteSizeY));
+				playerSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, 0, spriteSizeX, spriteSizeY));
 			}
 			fireSprite.setTextureRect(sf::IntRect(spriteSizeX * fireframe, 0, fspriteSizeX, fspriteSizeY));
 		fireframe++;
