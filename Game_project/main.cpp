8c6751e9241d4f13d0ff4af7de006286d1e9e420
t0npl <69316922+t0npl@users.noreply.github.com>
2020-12-15 08:25:58 +0700

    manyrhing

--- Game_project/main.cpp
+++ Game_project/main.cpp
@@ -4,11 +4,13 @@
 #include<vector>
 #include<stdio.h>
 using namespace sf;
+
 //functionprototype;;
 void shoot(float, float);
 void shot(float, float);
 void enemyGenerate(float);
 void BE1Checkcollision();
+
 //bulletclass;;
 class Bulleted {
 public:
@@ -25,9 +27,10 @@ public:
 	{ 
 		return bullet.getPosition(); 
 	}
-	//Collider GetCollider() { return Collider(bullet); }
+	FloatRect GetGlobleBounds() { return bullet.getGlobalBounds(); };
 };
 Bulleted bullet[6];
+
 //enemyclass;;
 class Enemy01 {
 public :
@@ -57,8 +60,13 @@ public :
 	{
 		return enemy1.getPosition();
 	}
+	FloatRect GetGlobleBounds() 
+	{ 
+		return enemy1.getGlobalBounds(); 
+	};
 };
 Enemy01 enemy1[10];
+
 //globalvariable;;
 int chk_1[6] = { 0,0,0,0,0,0 };
 int pst = 2;
@@ -67,36 +75,52 @@ int chksup_1[1] = { 0 };
 int pst1sup[1];
 int enemy1ch[10] = {0,0,0,0,0,0,0,0,0,0};
 int enemyRandomStatus;
+int bloodc = 4;
 clock_t start = -0.2, end = 0;
 Clock enemycl;
 Clock mainClock;
+Clock bloodClock;
+
 //mainfunction;;
 int main() {
+	
 	//generalvariable;;
 	float enemyTime;
+	
 	//renderwindow;;
 	sf::RenderWindow window(sf::VideoMode(1200, 600), "Pachara Loawpetch 63010629");
+	
 	//bgtexture;;
 	sf::Texture backgroundTexture;
 	if (!backgroundTexture.loadFromFile("bg.png"))
 	{
 		std::cout << "load failed" << std::endl;
 	}
+	
 	//playertexture;;
 	sf::Texture playerTexture;
 	if (!playerTexture.loadFromFile("player_plane.png"))
 	{
 		std::cout << "load failed" << std::endl;
 	}
+	
 	//firetexture;;
 	sf::Texture fireTexture;
 	if (!fireTexture.loadFromFile("fire.png"))
 	{
 		std::cout << "load failed" << std::endl;
 	}
+	
 	//bg;;
 	sf::Sprite backgroundSprite;
 	backgroundSprite.setTexture(backgroundTexture);
+
+	//blood;;
+	sf::RectangleShape blood;
+	blood.setFillColor(sf::Color::Red);
+	blood.setPosition(50,25);
+	blood.setSize(sf::Vector2f(200,25));
+	
 	//player;;
 	sf::Sprite playerSprite;
 	playerSprite.setTexture(playerTexture);
@@ -104,6 +128,7 @@ int main() {
 	int spriteSizeY = playerTexture.getSize().y/2;
 	playerSprite.setTextureRect(sf::IntRect(0, 0, spriteSizeX, spriteSizeY));
 	playerSprite.setPosition(150,50);
+	
 	//fire;;
 	sf::Sprite fireSprite;
 	fireSprite.setTexture(fireTexture);
@@ -111,6 +136,14 @@ int main() {
 	int fspriteSizeY = playerTexture.getSize().y / 1;
 	fireSprite.setTextureRect(sf::IntRect(0, 0, fspriteSizeX, fspriteSizeY));
 	fireSprite.setPosition(90, 57);
+	
+	//gameover
+	sf::RectangleShape gameover;
+	gameover.setSize(sf::Vector2f(1200, 600));
+	sf::Texture overTexture;
+	overTexture.loadFromFile("Gameover.jpg");
+	gameover.setTexture(&overTexture);
+	
 	//generalvariables;;
 	int animationFrame = 0;
 	int fireframe=0;
@@ -119,101 +152,214 @@ int main() {
 	int i = 0;
 	int frameNumber=0;
 	float ttime;
+	float bloodTime;
 	int timer;
-	//generalCommands
-	//printf("%d\n", frameNumber);
+	int state=0;
+	
+	//Fontsetting;;
+	Font gameFont;
+	gameFont.loadFromFile("FrostbiteBossFight-dL0Z.ttf");
+	Text START;
+	Text HIGHSCORE;
+	Text EXIT;
+	
+	//STARTatmenu
+	START.setFont(gameFont);
+	START.setOutlineColor(Color::Black);
+	START.setFillColor(Color::White);
+	START.setStyle(Text::Regular);
+	START.setString("START");
+	START.setCharacterSize(35);
+	START.setPosition(150, 200);
+
+	//HIFHGSCOREatmenu
+	HIGHSCORE.setFont(gameFont);
+	HIGHSCORE.setOutlineColor(Color::Black);
+	HIGHSCORE.setFillColor(Color::White);
+	HIGHSCORE.setStyle(Text::Regular);
+	HIGHSCORE.setString("HIGHSCORE");
+	HIGHSCORE.setCharacterSize(35);
+	HIGHSCORE.setPosition(150, 300);
+	
+	//EXITatmenu
+	EXIT.setFont(gameFont);
+	EXIT.setOutlineColor(Color::Black);
+	EXIT.setFillColor(Color::White);
+	EXIT.setStyle(Text::Regular);
+	EXIT.setString("EXIT");
+	EXIT.setCharacterSize(35);
+	EXIT.setPosition(150, 400);
+
 	//gameloop;;
 	while (window.isOpen())
 	{
-		int a;
-		//randomNumber;;
-		srand(time(NULL));
-		a=rand();
-		a=rand();
-		//other;;
-		ttime = mainClock.getElapsedTime().asSeconds();
-		timer = ttime;
-		enemyTime = enemycl.getElapsedTime().asSeconds();
-		movement = 0;
-		if (enemyTime > 7.000) {
-			enemycl.restart();
-		}
-		sf::Vector2f pos = playerSprite.getPosition();
-		shoot(pos.x, pos.y);
-		shot(pos.x, pos.y);
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-		{
-			movement = 2;
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-		{
-			movement = 1;
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-		{
-			window.close();
-		}
-		///////////////////;;
-		if (movement == 0) {
-			playerSprite.setTextureRect(sf::IntRect(0, 0, spriteSizeX, spriteSizeY));
-		}
-		else if (movement == 2 && yBorder <= 530) {
-			playerSprite.move(0.f, 2.f);
-			fireSprite.move(0.f, 2.f);
-			yBorder += 2;
-			playerSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 1, spriteSizeX, spriteSizeY));
-		}
-		else if (movement == 1 && yBorder >= 0) {
-			playerSprite.move(0.f, -2.f);
-			fireSprite.move(0.f, -2.f);
-			yBorder -= 2;
-			playerSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, 0, spriteSizeX, spriteSizeY));
-		}
-		fireSprite.setTextureRect(sf::IntRect(spriteSizeX * fireframe, 0, fspriteSizeX, fspriteSizeY));
-		fireframe++;
-		if (fireframe >= 3) {
-			fireframe = 0;
-		}
-		animationFrame++;
-		if (animationFrame >= 2) {
-			animationFrame = 2;
-		}
-		/////////////////;;
-		window.draw(backgroundSprite);
-		window.draw(playerSprite);
-		window.draw(fireSprite);
-		for (int i = 0; i < 6; i++) {
-			if (chk_1[i] == 1) {
-				window.draw(bullet[i].bullet);
-			}
-		}
-		//enemymove;;
-		enemyGenerate(enemyTime);
-		for (int i = 0; i < 10; i++) {
-			float enemyPosition = enemy1[i].enemy1.getPosition().x;
-			if (enemyPosition < 0) {
-				enemy1ch[i] = 0;
-			}
-		}
-		for (int i = 0; i <= 10; i++) {
-			if (enemy1ch[i] == 1) {
-				window.draw(enemy1[i].enemy1);
-			}
-		}
-		for (int i = 0; i < 10; i++) {
-			if (enemy1ch[i] == 1) {
-				float speed = .5f;
-				enemy1[i].enemy1.move(-speed, 0);
-				enemy1[i].animation(enemyTime);
+		//menu;;
+		if (state == 0) {
+
+			//menubg;;
+			sf::RectangleShape menubg;
+			menubg.setSize(sf::Vector2f(1200, 600));
+			sf::Texture menuTexture;
+			menuTexture.loadFromFile("menubg.jpeg");
+			menubg.setTexture(&menuTexture);
+			window.draw(menubg);
+
+			//menuButton
+			Vector2i mouse = Mouse::getPosition(window);
+			if (mouse.x > 151 and mouse.x < 220 and mouse.y>209 and mouse.y < 234) {
+				if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+					state = 1;
 				}
+			}
+
+			if (mouse.x > 151 and mouse.x < 291 and mouse.y > 316 and mouse.y < 335) {
+
+			}
+
+			if (mouse.x > 151 and mouse.x < 200 and mouse.y > 411 and mouse.y < 437)
+			{
+				if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) 
+				{
+					window.close();
+				}
+			}
+
+			window.draw(START);
+			window.draw(HIGHSCORE);
+			window.draw(EXIT);
+			
+			//windowcommand;;
+			window.display();
+			window.clear();
 		}
+		
+		//playingstate
+		else if (state == 1) {
+			int a;
 
-		BE1Checkcollision();
+			//randomNumber;;
+			srand(time(NULL));
+			a = rand();
+			a = rand();
 
-		//windowcommands;;
-		window.display();
-		window.clear();
-		frameNumber++;
+			//other;;
+			ttime = mainClock.getElapsedTime().asSeconds();
+			timer = ttime;
+			enemyTime = enemycl.getElapsedTime().asSeconds();
+			bloodTime = bloodClock.getElapsedTime().asSeconds();
+			movement = 0;
+			if (enemyTime > 7.000) {
+				enemycl.restart();
+			}
+
+			sf::Vector2f pos = playerSprite.getPosition();
+			shoot(pos.x, pos.y);
+			shot(pos.x, pos.y);
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+			{
+				movement = 2;
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+			{
+				movement = 1;
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+			{
+				window.close();
+			}
+
+			if (movement == 0) {
+				playerSprite.setTextureRect(sf::IntRect(0, 0, spriteSizeX, spriteSizeY));
+			}
+			else if (movement == 2 && yBorder <= 530) {
+				playerSprite.move(0.f, 2.f);
+				fireSprite.move(0.f, 2.f);
+				yBorder += 2;
+				playerSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 1, spriteSizeX, spriteSizeY));
+			}
+			else if (movement == 1 && yBorder >= 0) {
+				playerSprite.move(0.f, -2.f);
+				fireSprite.move(0.f, -2.f);
+				yBorder -= 2;
+				playerSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, 0, spriteSizeX, spriteSizeY));
+			}
+			fireSprite.setTextureRect(sf::IntRect(spriteSizeX * fireframe, 0, fspriteSizeX, fspriteSizeY));
+			fireframe++;
+			if (fireframe >= 3) {
+				fireframe = 0;
+			}
+			animationFrame++;
+			if (animationFrame >= 2) {
+				animationFrame = 2;
+			}
+
+			window.draw(backgroundSprite);
+			window.draw(playerSprite);
+			window.draw(fireSprite);
+			for (int i = 0; i < 6; i++) {
+				if (chk_1[i] == 1) {
+					window.draw(bullet[i].bullet);
+				}
+			}
+
+			//blood
+			if (bloodc == 3) {
+				blood.setSize(sf::Vector2f(150, 25));
+			}
+			else if (bloodc == 2) {
+				blood.setSize(sf::Vector2f(100, 25));
+			}
+			else if (bloodc == 1) {
+				blood.setSize(sf::Vector2f(50, 25));
+			}
+
+			//enemymove;;
+			enemyGenerate(enemyTime);
+			for (int i = 0; i < 10; i++) {
+				float enemyPosition = enemy1[i].enemy1.getPosition().x;
+				if (enemyPosition < 0) {
+					enemy1ch[i] = 0;
+					if (bloodTime > 3) {
+						bloodc--;
+						bloodClock.restart();
+					}
+				}
+			}
+
+			for (int i = 0; i <= 10; i++) {
+				if (enemy1ch[i] == 1) {
+					window.draw(enemy1[i].enemy1);
+				}
+			}
+
+			for (int i = 0; i < 10; i++) {
+				if (enemy1ch[i] == 1) {
+					float speed = .5f;
+
+					//enemymovement;;
+					if (timer > 50) {
+						speed = timer / 100;
+					}
+					enemy1[i].enemy1.move(-speed, 0);
+					enemy1[i].animation(enemyTime);
+				}
+			}
+			BE1Checkcollision();
+			if (bloodc != 0) {
+				window.draw(blood);
+			}
+			if (bloodc == 0) {
+				window.draw(gameover);
+			}
+			if (bloodc < 0) {
+				bloodc = 0;
+			}
+
+			//windowcommands;;
+			window.display();
+			window.clear();
+			frameNumber++;
+		}
 	}
 	return 0;
 }
@@ -265,47 +411,15 @@ void enemyGenerate(float enemyTime) {
 }
 void BE1Checkcollision() {
 	for (int i = 0; i < 10; i++) {
-		if (bullet[0].GetPosition().y - enemy1[i].GetPosition().y < 30. && bullet[0].GetPosition().y - enemy1[i].GetPosition().y > -32.&&enemy1[i].GetPosition().x-bullet[0].GetPosition().x<=30.) 
+		for (int j = 0; j < 6; j++)
 		{
-			chk_1[0] = 0;
-			enemy1ch[i] = 0;
-			bullet[0].set(-100, 100);
-			enemy1[i].set(1300, 700,1);
-		}
-		if (bullet[1].GetPosition().y - enemy1[i].GetPosition().y < 30. && bullet[1].GetPosition().y - enemy1[i].GetPosition().y > -32. && enemy1[i].GetPosition().x - bullet[1].GetPosition().x <= 30.)
-		{
-			chk_1[1] = 0;
-			enemy1ch[i] = 0;
-			bullet[1].set(-100, 100);
-			enemy1[i].set(1300, 700, 1);
-		}
-		if (bullet[2].GetPosition().y - enemy1[i].GetPosition().y < 30. && bullet[2].GetPosition().y - enemy1[i].GetPosition().y > -32. && enemy1[i].GetPosition().x - bullet[2].GetPosition().x <= 30.)
-		{
-			chk_1[2] = 0;
-			enemy1ch[i] = 0;
-			bullet[2].set(-100, 100);
-			enemy1[i].set(1300, 700, 1);
-		}
-		if (bullet[3].GetPosition().y - enemy1[i].GetPosition().y < 30. && bullet[3].GetPosition().y - enemy1[i].GetPosition().y > -32. && enemy1[i].GetPosition().x - bullet[3].GetPosition().x <= 30.)
-		{
-			chk_1[3] = 0;
-			enemy1ch[i] = 0;
-			bullet[3].set(-100, 100);
-			enemy1[i].set(1300, 700, 1);
-		}
-		if (bullet[4].GetPosition().y - enemy1[i].GetPosition().y < 30. && bullet[4].GetPosition().y - enemy1[i].GetPosition().y > -32. && enemy1[i].GetPosition().x - bullet[4].GetPosition().x <= 30.)
-		{
-			chk_1[4] = 0;
-			enemy1ch[i] = 0;
-			bullet[4].set(-100, 100);
-			enemy1[i].set(1300, 700, 1);
-		}
-		if (bullet[5].GetPosition().y - enemy1[i].GetPosition().y < 30. && bullet[5].GetPosition().y - enemy1[i].GetPosition().y > -32. && enemy1[i].GetPosition().x - bullet[5].GetPosition().x <= 30.)
-		{
-			chk_1[5] = 0;
-			enemy1ch[i] = 0;
-			bullet[5].set(-100, 100);
-			enemy1[i].set(1300, 700, 1);
+			if (bullet[j].GetGlobleBounds().intersects(enemy1[i].GetGlobleBounds()))
+			{
+				chk_1[j] = 0;
+				enemy1ch[i] = 0;
+				bullet[j].set(-100, 100);
+				enemy1[i].set(1300, 700, 1);
+			}
 		}
 	}
 }
